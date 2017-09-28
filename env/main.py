fc18c04ce5e78441cb1f543020c9d1db7fa1a579
shrimptoast12 <viet.tran714@gmail.com>
2017-09-27 19:19:07 -0500

    Update main.py

--- env/main.py
+++ env/main.py
@@ -124,10 +124,4 @@ def twisted_treeline():
 @app.route('/urgot-single.html')
 def urgot_single():
     return render_template('urgot-single.html')
-
-@app.errorhandler(500)
-def server_error(e):
-    # Log the error and stacktrace.
-    logging.exception('An error occurred during a request.')
-    return 'An internal error occurred.', 500
 # [END app]
