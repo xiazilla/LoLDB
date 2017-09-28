6d87938d46c0b71d4123e3c1fb588eafd17c78f2
shrimptoast12 <viet.tran714@gmai.com>
2017-09-27 22:26:37 -0500

    testing if GCP works with static folder

---
+++ env/static/js/header.js
@@ -0,0 +1,35 @@
+
+ document.getElementById("top-bar").innerHTML =   
+        '<div class="container">'+
+            '<div class="navbar-header">'+
+                '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
+                '<span class="sr-only">Toggle navigation</span>'+
+                '<span class="icon-bar"></span>'+
+                '<span class="icon-bar"></span>'+
+                '<span class="icon-bar"></span>'+
+                '</button>'+
+
+                '<div class="navbar-brand">'+
+                    '<a href="index.html" >'+
+                        '<img src="images/logo.png" alt="">'+
+                    '</a>'+
+                '</div>'+
+
+            '</div>'+
+
+            '<nav class="collapse navbar-collapse navbar-right" role="navigation">'+
+                '<div class="main-menu">'+
+                    '<ul class="nav navbar-nav navbar-right">'+
+                        '<li>'+
+                            '<a href="index.html" >Home</a>'+
+                        '</li>'+
+                        '<li><a href="champions.html">Champions</a></li>'+
+			            '<li><a href="items.html">Items</a></li>'+
+                        '<li><a href="matches.html">Matches</a></li>'+
+                        '<li><a href="game-modes.html">Game Modes</a></li>'+
+                        '<li><a href="about.html">About</a></li>'+
+                    '</ul>'+
+                '</div>'+
+            '</nav>'+
+
+        '</div>';    
