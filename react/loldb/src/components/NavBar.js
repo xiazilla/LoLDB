d2b009c092f960b1b8d7dc5570a068c8694e7ee3
xiazilla <jxia@wireless-10-147-230-161.public.utexas.edu>
2017-10-20 17:01:05 -0500

    NavBar Component is done

--- react/loldb/src/components/NavBar.js
+++ react/loldb/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import logo from "../images/logo.png";
 
 export default class LoLNav extends React.Component {
   constructor(props) {
@@ -19,15 +20,29 @@ export default class LoLNav extends React.Component {
     return (
       <div className="NavBar">
         <Navbar color="faded" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarBrand href="/">
+          <img src={logo}/>
+          </NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+              	<NavLink href="/">Home</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+                <NavLink href="/">Champions</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Items</NavLink>
+              </NavItem>
+              <NavItem>
+              	<NavLink href="/">Matches</NavLink>
+              </NavItem>
+              <NavItem>
+              	<NavLink href="/">Game Modes</NavLink>
+              </NavItem>
+              <NavItem>
+              	<NavLink href="/">About</NavLink>
               </NavItem>
             </Nav>
           </Collapse>
