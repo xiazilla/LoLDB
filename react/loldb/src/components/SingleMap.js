7dd9c1ecb0aef5a7d747a807587b67f1d8dfae07
Anjiya Nayani <anjiyanayani@utexas.edu>
2017-10-25 22:41:55 -0500

    updated single map page, need to add article text and names

--- react/loldb/src/components/SingleMap.js
+++ react/loldb/src/components/SingleMap.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import myMaps from './maps.json';
+import myItems from './items.json';
+import myChamps from './champions.json'
 
 class SingleMap extends Component{
 
@@ -11,7 +13,7 @@ class SingleMap extends Component{
 		var text = div.textContent || div.innerText || "";
 		return text;
 	}
-	
+
 	render() {
 	
 
@@ -37,21 +39,45 @@ class SingleMap extends Component{
 		}
 		console.log(myMaps);
 		console.log(mapIdent)
+
 		let data = myMaps;
+
 		Object.keys(data).forEach(function(key, index) {
 			if(data[key].mapName === mapIdent) 
 				mapData = data[key];
 		});		
-	
+
+		console.log(mapData); 
+		
+		let article = mapData.article;
+		console.log(article);
+		let section = article.section;
+
+
+		let imageitemUrl ="https://ddragon.leagueoflegends.com/cdn/7.10.1/img/item/";
+		let imagechampionUrl = "https://ddragon.leagueoflegends.com/cdn/7.20.1/img/champion/";
+
 		return (
 			<div> 
 				<h3><strong>{mapIdent}</strong></h3>
 				<div className="img-wrapper2>" >
 	                <img src={mapData.image} className="img-responsive" alt="portfolio items" />
 				</div>
-				<section>
+				<h4>Articles</h4>
+				<p>{mapData.article.section}</p>
 
-				</section>
+				<h4>Champions on this Map</h4>
+				
+					<div className = "row-md-10"> {mapData.champs.map(champion => 
+        				<a href={`/Champions/${champion}`} >  <img src={(imagechampionUrl).concat(champion + ".png") } alt="" />   </a>)}
+        			</div>
+        		
+
+				<h4>Items on this Map</h4>
+					<div className = "row-md-8"> {mapData.items.map(item => 
+        				<a href={`/Items/${item}`} >  <img src={(imageitemUrl).concat(item + ".png")} alt=""/>  </a>)}
+        			</div>
+
 			</div>
 		)
 	}
