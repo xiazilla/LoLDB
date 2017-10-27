9afb8f97d9ead1a7a2f61b0d235e748572b62eb6
Anjiya Nayani <anjiyanayani@utexas.edu>
2017-10-26 19:02:54 -0500

    about page update

--- react/loldb/src/components/About.js
+++ react/loldb/src/components/About.js
@@ -43,9 +43,9 @@ class About extends Component {
                             <h4>Responsibilities:</h4>
                                 <p>Backend</p>
                                 
-                            <h4>Number of Commits: 11</h4>
-                            <h4>Number of Issues: 5</h4>
-                            <h4>Number of Unit Tests: 0</h4>
+                            <h5>Number of Commits: 11</h5>
+                            <h5>Number of Issues: 5</h5>
+                            <h5>Number of Unit Tests: 0</h5>
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -59,9 +59,9 @@ class About extends Component {
                             <h4>Responsibilities:</h4>
                                 <p>Frontend and Backend</p>
 
-                            <h4>Number of Commits: 11</h4>
-                            <h4>Number of Issues: 5</h4>
-                            <h4>Number of Unit Tests: 0</h4>
+                            <h5>Number of Commits: 11</h5>
+                            <h5>Number of Issues: 5</h5>
+                            <h5>Number of Unit Tests: 0</h5>
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -74,9 +74,9 @@ class About extends Component {
                             Doesn't actually play League.</p>
                             <h4>Responsibilities:</h4>
                                 <p>Backend</p>
-                            <h4>Number of Commits: 11</h4>
-                            <h4>Number of Issues: 5</h4>
-                            <h4>Number of Unit Tests: 0</h4>
+                            <h5>Number of Commits: 11</h5>
+                            <h5>Number of Issues: 5</h5>
+                            <h5>Number of Unit Tests: 0</h5>
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -88,9 +88,9 @@ class About extends Component {
                             <p> Senior in Computer Science. Allergic to weakness. And Bronzies.</p>
                             <h4>Responsibilities:</h4>
                                 <p>Frontend</p>
-                            <h4>Number of Commits: 11</h4>
-                            <h4>Number of Issues: 5</h4>
-                            <h4>Number of Unit Tests: 0</h4>
+                            <h5>Number of Commits: 11</h5>
+                            <h5>Number of Issues: 5</h5>
+                            <h5>Number of Unit Tests: 0</h5>
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -103,20 +103,20 @@ class About extends Component {
                             League of Legends because of this project.</p>
                             <h4>Responsibilities:</h4>
                                 <p>Frontend</p>
-                            <h4>Number of Commits: 11</h4>
-                            <h4>Number of Issues: 5</h4>
-                            <h4>Number of Unit Tests: 0</h4>
+                            <h5>Number of Commits: 11</h5>
+                            <h5>Number of Issues: 5</h5>
+                            <h5>Number of Unit Tests: 0</h5>
                         </div>
                     </div>
                 </div>
             <div id="call-to-action">
             <div class="container">
-                <div class="row">
+                <div class="row-md-12">
                     <div class="col-md-12">
                         <div class="block">
                             <h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">Tools </h2>
                             <h5> <b>Apiary:</b> A useful web app that provides framework to easily document your endpoint’s API calls. </h5>
-                            <h5> <b>Bootstrap:</b> A front-end development framework made up of HTML, CSS, and Javascript that allows the build of fully responsive websites. </h4>
+                            <h5> <b>Bootstrap:</b> A front-end development framework made up of HTML, CSS, and Javascript that allows the build of fully responsive websites. </h5>
                             <h5> <b>Flask:</b> A Python framework used to create our back-end API </h5>
                             <h5> <b>Github:</b> Our source control application of choice. </h5>
                             <h5> <b>Google Cloud Platform:</b> To host our static website on the Google Cloud Storage. </h5>
@@ -126,8 +126,8 @@ class About extends Component {
                             <h5> <b>Trello:</b> For issue tracking. </h5>
 
                             <h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">Data </h2>
-                            <h4><a href="https://developer.riotgames.com/">Riot Games API:</a> the best place for any GET requests about League of Legends information.</h4>
-                            <h4><a href="http://leagueoflegends.wikia.com/api/v1">League of Legends Fandom Wiki API:</a> the only other place for any GET requests about League of Legends.</h4>
+                            <h5><a href="https://developer.riotgames.com/">Riot Games API:</a> the best place for any GET requests about League of Legends information.</h5>
+                            <h5><a href="http://leagueoflegends.wikia.com/api/v1">League of Legends Fandom Wiki API:</a> the only other place for any GET requests about League of Legends.</h5>
 
 
                             <h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">Stats </h2>
