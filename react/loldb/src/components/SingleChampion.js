25e3cdace74173c20b7bb5f04b9ff8fa0544f1fe
Jamie Xia <jamie88xia@utexas.edu>
2017-11-03 16:39:47 -0500

    champion single updated json

--- react/loldb/src/components/SingleChampion.js
+++ react/loldb/src/components/SingleChampion.js
@@ -47,7 +47,7 @@ class AllItems extends Component {
 class SingleChampion extends Component {
 
 	GetHtml( theJSON ) {
-		var html = theJSON.description;
+		var html = theJSON;
     	var div = document.createElement("div");
 		div.innerHTML = html;
 		var text = div.textContent || div.innerText || "";
@@ -59,110 +59,119 @@ class SingleChampion extends Component {
 		super(props);
 		this.state = {
 			champName: props.match.params.name,
-			champData: []
+			champData: [],
+			champDataLoaded: 0
 		}
 	}
 
-	componentDidMount() {
-		// console.log(this.state.champName)    
-	  var url = `https://loldbapi.appspot.com/api/champs/${this.state.champName}`
+	componentWillMount() {
+		let url = `https://loldbapi.appspot.com/api/champs/${this.props.match.params.name}`;
 
-    axios.get(url)
-      .then(res => {
-        console.log(res)
-      });
+		fetch(url)
+		.then(response => {
+			return response.json()
+			}).then(results => {
+				console.log(results.result[0])
+				this.setState({champData: results.result[0]})
+				this.setState({champDataLoaded: 1})
+				// this.render()
+				// console.log("state", this.state.data)
+			})
+
+
 	}
 
 	render() {
-
-		let championData, spellData;
-		let q,w,e,r,p, itemsChampName;
-		var champName = this.props.match.params.name;
-		console.log(champName)
-    	let data = mydata.data;
-    	var skins = [];
-    	let recommendedItems = []
-
-    	Object.keys(data).forEach(function(key) {
-      		if(data[key].name === champName){
-      			itemsChampName = key;
-      			championData = data[key];
-      			spellData = championData.spells
-      			// console.log(championData.skins);
-				Object.keys(championData.skins).forEach(function(key) {
-			  		skins.push(championData.skins[key]);
-				});      			
-				q = spellData[0]
-      			w = spellData[1]
-      			e = spellData[2]
-      			r = spellData[3]
-      			p = championData.passive
-    			recommendedItems = championData.recommended
-    			// console.log(recommendedItems)
-      		};
-    	});
+		if(this.state.champDataLoaded === 0) {
+			return (<div>loading...</div>)
+		} else {
 
 
-    	let lore = championData.lore;
+			console.log("RENDER")
+			// console.log(this.state.champData)
+			let championData, spellData;
+			let q,w,e,r,p, itemsChampName;
+	    	var skins = [];
+	    	let recommendedItems = []
 
-    	let imageURL = "https://ddragon.leagueoflegends.com/cdn/7.20.1/img/spell/";
-    	let passiveURL = "https://ddragon.leagueoflegends.com/cdn/7.20.1/img/passive/";
-    	// let itemURL = ""
-    	// console.log((imageURL).concat(q.image.full));
-    	console.log(recommendedItems)
+  			itemsChampName = this.state.champName;
+  			championData = this.state.champData;
+  			spellData = championData.spells
+  			console.log(spellData)
+			Object.keys(championData.skins).forEach(function(key) {
+		  		skins.push(championData.skins[key]);
+			});      			
+			q = spellData[0]
+  			w = spellData[1]
+  			e = spellData[2]
+  			r = spellData[3]
+  			p = championData.passive
+			recommendedItems = championData.recommended
+	    			console.log(recommendedItems)
 
-		return (
-			<div>
-	            <div className="img-wrapper2">
-	                <img src={championData.image} className="img-responsive" alt="portfolio items" />
-	            </div>
-				<h3><strong>{champName}</strong></h3>
-				<h4>{championData.title}</h4> 
 
-				<h4> Champion Abilities</h4>
-				<div className="container1 row">
-					<div className="col-sm-3">
-						<img src={(passiveURL).concat(p.image.full)} className="img-responsive" alt="portfolio items" title = {this.GetHtml(p)}/> 
-						<p><strong>{p.name}</strong></p>
-					</div>
-					<div className="col-sm-2">
-						<img src={(imageURL).concat(q.image.full)} className="img-responsive" alt="" title={this.GetHtml(q)}/> 
-						<p><strong>{q.name}</strong></p>
-					</div>
-					<div className="col-sm-2">
-						<img src={(imageURL).concat(w.image.full)} className="img-responsive" alt="" title={this.GetHtml(w)}/> 
-						<p><strong>{w.name}</strong></p>
-					</div>
-					<div className="col-sm-2">
-						<img src={(imageURL).concat(e.image.full)} className="img-responsive" alt="" title={this.GetHtml(e)}/> 
-						<p><strong>{e.name}</strong></p>
-					</div>
-					<div className="col-sm-2">
-						<img src={(imageURL).concat(r.image.full)} className="img-responsive" alt="" title={this.GetHtml(r)}/> 
-						<p><strong>{r.name}</strong></p>
-					</div>					
-				</div>
 
-				<h4> Skins </h4>
-		    	<div className = "row"> {skins.map((skin) => 
-					<SkinObject key={skin.title} thisSkin={skin} name={itemsChampName} index={skin.id % 10} />)}
-		    	</div>
+	    	let lore = championData.lore;
 
-				<h4>Recommended Items</h4>
+	    	let imageURL = "https://ddragon.leagueoflegends.com/cdn/7.20.1/img/spell/";
+	    	let passiveURL = "https://ddragon.leagueoflegends.com/cdn/7.20.1/img/passive/";
+	    	// let itemURL = ""
+	    	// console.log((imageURL).concat(q.image.full));
+	    	console.log(championData.image)
 
+			return (
 				<div>
-						<AllItems recommended={recommendedItems}/>
-				</div>
+		            <div className="img-wrapper2">
+		                <img src={championData.image} className="img-responsive" alt="portfolio items" />
+		            </div>
+					<h3><strong>{this.state.champName}</strong></h3>
+					<h4>{championData.title}</h4> 
 
-				<h4>Champion Lore</h4>
-				<div className="row">
-					<div className="col-sm-1"></div>
-					<div className="col-sm-10">
-						<p>{document.write(lore)}</p>
+					<h4> Champion Abilities</h4>
+					<div className="container1 row">
+						<div className="col-sm-3">
+							<img src={(passiveURL).concat(p.image.full)} className="img-responsive" alt="portfolio items" title = {this.GetHtml(p)}/> 
+							<p><strong>{p.name}</strong></p>
+						</div>
+						<div className="col-sm-2">
+							<img src={(imageURL).concat(q.image)} className="img-responsive" alt="" title={this.GetHtml(q.description)}/> 
+							<p><strong>{q.name}</strong></p>
+						</div>
+						<div className="col-sm-2">
+							<img src={(imageURL).concat(w.image)} className="img-responsive" alt="" title={this.GetHtml(w.description)}/> 
+							<p><strong>{w.name}</strong></p>
+						</div>
+						<div className="col-sm-2">
+							<img src={(imageURL).concat(e.image)} className="img-responsive" alt="" title={this.GetHtml(e.description)}/> 
+							<p><strong>{e.name}</strong></p>
+						</div>
+						<div className="col-sm-2">
+							<img src={(imageURL).concat(r.image)} className="img-responsive" alt="" title={this.GetHtml(r.description)}/> 
+							<p><strong>{r.name}</strong></p>
+						</div>					
+					</div>
+
+					<h4> Skins </h4>
+			    	<div className = "row"> {skins.map((skin) => 
+						<SkinObject key={skin.title} thisSkin={skin} name={itemsChampName} index={skin.id % 10} />)}
+			    	</div>
+
+					<h4>Recommended Items</h4>
+
+					<div>
+							<AllItems recommended={recommendedItems}/>
+					</div>
+
+					<h4>Champion Lore</h4>
+					<div className="row">
+						<div className="col-sm-1"></div>
+						<div className="col-sm-10">
+							<p>{this.GetHtml(lore)}</p>
+						</div>
 					</div>
 				</div>
-			</div>
-		) 
+			) 
+		} 
 	}
 }
 
