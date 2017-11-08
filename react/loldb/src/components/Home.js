66d27a317954456c5cb39332aa2fc4f9e7518e39
Viet Tran <viettran@Viets-iMac.local>
2017-11-08 12:25:36 -0600

    Added twitter feed and made skib carousel relative

--- react/loldb/src/components/Home.js
+++ react/loldb/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import { Timeline } from 'react-twitter-widgets';
 import background from "../../src/images/LeagueBackground.jpg"
 import background2 from "../../src/images/LeagueBackground4.jpeg"
 import background3 from "../../src/images/LeagueBackground3.jpg"
@@ -22,6 +23,9 @@ class DemoCarousel extends Component {
 	                    <img src={background3} alt=""/>
 	                </div>
 	            </Carousel>
+	            <Timeline dataSource={{sourceType: 'https://twitter.com/lolesports',
+										   screenName: 'lolesports'}}
+		  					  options={{height: '2000'}}/>
 	        </div>
         )
     }
@@ -31,12 +35,7 @@ class Home extends Component {
 	render() {
 		return (
 			<div>
-				<div>
 					<DemoCarousel/>
-				</div>
-				<div className="col-md-12">
-                        <a className="twitter-timeline" data-width="800" href="https://twitter.com/lolesports">Tweets by lolesports</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                </div>
 			</div>
 
 		)
