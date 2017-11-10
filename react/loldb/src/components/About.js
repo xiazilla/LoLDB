fe4e59aa034bcd6c9e179818aae225b0f8ed00cf
Anjiya Nayani <anjiyanayani@utexas.edu>
2017-11-09 19:17:37 -0600

    fixed table alignment for single champion

--- react/loldb/src/components/About.js
+++ react/loldb/src/components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
             </div>
             </div>
 
-            <br></br>
+        <br></br>
         <h2 align="left">Motivation</h2>
         <p>
             LoLDB was created for summoners to look up information 
