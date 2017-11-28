8f7e03a8c077a814ce3fa898f26348bfdc4a42d7
Jamie Xia <jamie88xia@utexas.edu>
2017-11-27 23:49:02 -0600

    visualization fix

--- react/loldb/src/components/SweTravelsVis.js
+++ react/loldb/src/components/SweTravelsVis.js
@@ -24,7 +24,7 @@ class SweVisualization extends Component {
 		.then(response => {
 			return response.json();
 		}).then(results => {
-			// console.log(results)
+			console.log(results)
 			this.setState({parks: results});
 		});
 
@@ -62,7 +62,7 @@ class SweVisualization extends Component {
 		if(this.state.dataLoaded === 0) {
 			return (<div>Loading...</div>)
 		} else {
-			// console.log(this.state.parks)
+			console.log(this.state.campground)
 			// console.log(this.state.parks[0])
 	        return (
 			    <div>
@@ -93,40 +93,47 @@ class SweVisualization extends Component {
 						{this.state.states.map(states => <ForceGraphNode key={states.abbreviations} node={{ id: states.abbreviations, label: states.name }} fill="orange" />)}
 
 						{this.state.campground.map(campground => <ForceGraphLink link={{ source: campground.name, target: campground.parkCode }} />)}
-						{this.state.campground.map(function(campground) {
-							if(campground.states === 'CA,NV') {
-								return <div></div>
-							}
-							return <ForceGraphLink link={{ source: campground.name, target: campground.states }} />
-						})}
-						{this.state.campground.map(function(campground) {
-							if(campground.states === 'CA,NV') {
-								return <div></div>
-							}
-							return <ForceGraphLink link={{ source: campground.name, target: campground.states }} />
-						})}
+
 						{this.state.visCen.map(function(visCen){
 							let state = visCen.states;
 							if(visCen.states === 'VI') {
 								state = 'VA'
 							}
+							// console.log("STATE " + state)
 							return <ForceGraphLink link={{ source: visCen.name, target: state}} />
 						} )}
+
+						{this.state.campground.map(function(campground) {
+							if(campground.states.length === 2) {
+								if(campground.states !== 'DC' && campground.states !== 'AS' && campground.states !== 'GU' && campground.states !== 'VI') {
+									return <ForceGraphLink link={{source: campground.name, target: campground.states}} />
+								} 
+								return <div></div>
+							} else {
+									var array = campground.states.split(',');
+									return array.map(
+										function(state) {
+											if (state !== 'DC' && state !== 'AS' && state !== 'GU' && state !== 'VI') {
+												return <ForceGraphLink link={{ source: campground.name, target: state}} />
+											} else {
+												return <div></div>
+											}
+										})
+							}
+
+						})}
 
 						{this.state.visCen.map(function(visCen){
 							return <ForceGraphLink link={{ source: visCen.name, target: visCen.parkCode}} />
 						} )}
 						{this.state.parks.map(
 							function(park) {
-								console.log()
 								if(park.states.length !== 2 ) {
 
 									var array = park.states.split(',');
 									return array.map(
 										function(state) {
 											if (state !== 'DC' && state !== 'AS' && state !== 'GU' && state !== 'VI') {
-												console.log(state)
-												console.log(park.parkCode)
 												return <ForceGraphLink link={{ source: park.parkCode, target: state}} />
 											} else {
 												return <div></div>
@@ -134,6 +141,7 @@ class SweVisualization extends Component {
 										})
 								}
 								if(park.states !== 'DC' && park.states !== 'AS' && park.states !== 'GU' && park.states !== 'VI') {
+
 									return <ForceGraphLink link={{source: park.parkCode, target: park.states}} />
 								} 
 								return <div></div>
