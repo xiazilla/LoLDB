13776406144518aae62a7a8bc429bbb85bb41040
Jamie Xia <jamie88xia@utexas.edu>
2017-11-27 19:06:43 -0600

    added description to visualization

--- react/loldb/src/index.js
+++ react/loldb/src/index.js
@@ -4,11 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render((
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
 ), document.getElementById('root'));
-registerServiceWorker();
