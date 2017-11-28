3c111a6b27b8806ec7084a5e533e09502640cfd2
Jamie Xia <jamie88xia@utexas.edu>
2017-11-27 19:35:50 -0600

    reverting to old routing

--- react/loldb/src/App.js
+++ react/loldb/src/App.js
@@ -11,7 +11,7 @@ import Search from './components/Search.js';
 import SweTravelsVis from './components/SweTravelsVis.js'
 
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 const Main = () => (
   <main>
@@ -32,22 +32,10 @@ const Main = () => (
 class App extends Component {
   render () {
     return (
-      <Router>
-        <div className='App'>
-          <LoLNav />
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/home' component={Home} />
-            <Route exact path='/about' component={About} />
-            <Route path='/champions' component={Champions} />
-            <Route path='/items' component={Items} />
-            <Route path='/matches' component={Matches} />
-            <Route path='/maps' component={Maps} />
-            <Route path='/searchResult=:searchFor' component={Search} />
-            <Route path='/SweTravelsVis' component={SweTravelsVis} />
-          </Switch>
-        </div>
-      </Router>
+      <div className='App'>
+        <LoLNav />
+        <Main />
+      </div>
     );
   }
 }
